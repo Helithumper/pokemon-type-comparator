a466d9dd192d91a54522001de58e82eaa710da22
Helithumper <peyton.duncan45@gmail.com>
2015-03-09 10:32:06 -0400

    Added Running Message

--- src/TypeChartRunner.java
+++ src/TypeChartRunner.java
@@ -3,6 +3,7 @@ public class TypeChartRunner {
 
 	public static void main(String[] args) {
 
+		System.out.println("Now Running");
 		MainFrame mainFrame = new MainFrame();
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
